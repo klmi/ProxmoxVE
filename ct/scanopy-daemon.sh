19fe5ec307da2f12d0e171197d9b4ad78fcc02bb
michael <michael@playground>
2026-01-06 15:17:44 +0100

    added service handling ater install

--- ct/scanopy-daemon.sh
+++ ct/scanopy-daemon.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2025 community-scripts ORG
+# Copyright (c) 2021-2026 community-scripts ORG
 # Author: klmi
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/scanopy/scanopy
 
 APP="Scanopy-Daemon"
-var_tags="${var_tags:-analytics}"
+var_tags="${var_tags:-scanopy}"
 var_cpu="${var_cpu:-1}"
 var_ram="${var_ram:-512}"
 var_disk="${var_disk:-3}"
@@ -22,11 +22,11 @@ catch_errors
 function update_script() {
    header_info
    check_container_storage
-#   check_container_resourcesy
-#   if [[ ! -d /opt/scanopy ]]; then
-#     msg_error "No ${APP} Installation Found!"
-#     exit
-#   fi
+   check_container_resourcesy
+   if [[ ! -d /opt/scanopy ]]; then
+     msg_error "No ${APP} Installation Found!"
+     exit
+   fi
 
    if check_for_gh_release "scanopy" "scanopy/scanopy"; then
      CLEAN_INSTALL=1 fetch_and_deploy_gh_release "scanopy" "scanopy/scanopy" "tarball" "latest" "/opt/scanopy"
--- install/scanopy-daemon-install.sh
+++ install/scanopy-daemon-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2025 community-scripts ORG
+# Copyright (c) 2021-2026 community-scripts ORG
 # Author: klmi
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/scanopy/scanopy
@@ -13,19 +13,83 @@ setting_up_container
 network_check
 update_os
 
-yes | bash /opt/scanopy/install.sh
-systemctl enable scanopy-daemon
-echo -e "⚠️  IMPORTANT: You must edit the service file with your daemon configuration:"
-echo -e ""
-echo -e "  sudo nano /etc/systemd/system/scanopy-daemon.service"
-echo -e ""
-echo -e "Add your daemon arguments to the ExecStart line:"
-echo -e "  ExecStart=/usr/local/bin/scanopy-daemon --server-url http://YOUR_SERVER --server-port 60072 --network-id YOUR_NETWORK_ID --daemon-api-key YOUR_API_KEY"
-echo -e ""
-echo -e "Then reload and start the service:"
-echo -e "  sudo systemctl daemon-reload"
+service_name="scanopy-daemon"
+service="/etc/systemd/system/$service_name.service"
+install_script="/opt/scanopy/install.sh"
 
+declare -A old_flags
+
+msg_info "Extract existing flags, in case service exists"
+if [[ -f "$service" ]]; then
+    msg_info "Extracting old ExecStart flags"
+    line=$(grep "ExecStart=/usr/local/bin/scanopy-daemon" "$service")
+    # all flags --flag value
+    while read -r flag value; do
+        old_flags["$flag"]="$value"
+    done < <(echo "$line" | grep -oP '--\S+ \S+' | awk '{print $1, $2}')
+    msg_ok "Old flags extracted"
+else
+    msg_info "No existing service, proceeding with fresh install"
+fi
+
+msg_info "Installing Scanopy Daemon"
+yes | bash "$install_script"
+msg_ok "Installation finished"
+
+msg_info "Rewrite formerly extracted values to service"
+if [[ -f "$service" && ${#old_flags[@]} -gt 0 ]]; then
+    msg_info "Applying old flag values to new service"
+
+    # read execstart_line from new file
+    execstart_line=$(grep "ExecStart=/usr/local/bin/scanopy-daemon" "$service")
+
+    # for each old flag
+    for flag in "${!old_flags[@]}"; do
+        value="${old_flags[$flag]}"
+        if [[ -z "$value" ]]; then
+            continue
+        fi
+
+        if grep -q "$flag" <<< "$execstart_line"; then
+            # flag exists in file -> write value
+            sed -i "s|${flag} [^ ]*|${flag} $value|" "$service"
+        else
+            # flag didn't exists -> append
+            sed -i "/ExecStart=/ s|$| $flag $value|" "$service"
+        fi
+    done
+    msg_ok "reloading daemon"
+    systemctl daemon-reload
+    systemctl enable $service_name
+
+    if systemctl list-units --all | grep -q "^$service_name.service"; then
+      if systemctl is-active --quiet "$service_name"; then
+        msg_ok "Old values applied successfully"
+	msg_ok "Service $service_name is running"
+      else
+        msg_error "Service $service_name exists but is not running"
+      fi
+    else
+      msg_error "Service $service_name does not exist"
+    fi
+
+else
+    msg_info "No old flags to apply"
+    msg_info ""
+    msg_info " IMPORTANT: You must edit the service file with your daemon configuration:"
+    msg_info ""
+    msg_info "  sudo nano /etc/systemd/system/scanopy-daemon.service"
+    msg_info ""
+    msg_info "Add your daemon arguments to the ExecStart line:"
+    msg_info ""
+    msg_info "  ExecStart=/usr/local/bin/scanopy-daemon --server-url http://YOUR_SERVER --server-port 60072 --network-id YOUR_NETWORK_ID --daemon-api-key YOUR_API_KEY"
+    msg_info ""
+    msg_info "Then reload and start the service:"
+    msg_info "  sudo systemctl daemon-reload"
+    msg_info ""
+fi
 
 motd_ssh
 customize
 cleanup_lxc
+
